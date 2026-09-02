3dbf00727d54173e87aca4a38adb59b6e4e627f6
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2026-09-02 17:55:13 +0530

    Feature: - Updated footer style to light. - Changed label from "Docs" to "Introduction" in footer links. - Updated copyright information in footer.
    Refactor:
    - Removed GitHub link from navbar.
    
    Fix:
    - Added styles for sidebar logo to ensure proper display.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -119,21 +119,16 @@ const config: Config = {
           label: "Docs",
         },
         { to: "/blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
-        },
       ],
     },
     footer: {
-      style: "dark",
+      style: "light",
       links: [
         {
           title: "Docs",
           items: [
             {
-              label: "Docs",
+              label: "Introduction",
               to: "/docs/introduction",
             },
           ],
@@ -169,7 +164,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Abhisek Mohanty. All rights reserved.`,
     },
     prism: {
       theme: prismThemes.gruvboxMaterialLight,
